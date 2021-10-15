9f2599b50187c47d6edbbabd3cba7471727cf7b2
root <root@DESKTOP-L0ORUNV.localdomain>
2021-10-15 15:08:06 +0200

    change button background color

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -5,17 +5,17 @@ export interface CustomTheme extends Theme {}
 export const theme: CustomTheme = createMuiTheme({
   palette: {
     primary: {
-      main: "#F9C41C"
+      main: "#4ECC22"
     }
   },
   overrides: {
     MuiButton: {
       root: {
         background:
-          "transparent linear-gradient(101deg, #F78E0D 0%, #F9C41C 100%) 0% 0% no-repeat padding-box"
+          "transparent linear-gradient(101deg, #F78E0D 0%, #4ECC22 100%) 0% 0% no-repeat padding-box"
       },
       contained: {
-        color: "green",
+        color: "white",
         boxShadow: "0px 2px 20px #00000029"
       },
       label: {
